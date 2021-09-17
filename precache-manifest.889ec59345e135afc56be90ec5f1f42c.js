296320f99634d930d498180fbf0521a0f48958e0
Code Sandbox <hello@codesandbox.io>
2021-09-17 09:29:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.889ec59345e135afc56be90ec5f1f42c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39cd0520f501f0b1e7aa280e08e1926",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/index.html"
+  },
+  {
+    "revision": "0ef1aacfe7c839cdbb31",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/css/main.c202a3f4.chunk.css"
+  },
+  {
+    "revision": "ef32cf81a6ab580483fc",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/js/2.3584594f.chunk.js"
+  },
+  {
+    "revision": "e964c20cd3a2e7340d693239bf02bc66",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/js/2.3584594f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef1aacfe7c839cdbb31",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/js/main.590ba34f.chunk.js"
+  },
+  {
+    "revision": "bb141c1e06aa56425199",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/js/runtime-main.cde65c0c.js"
+  },
+  {
+    "revision": "d2defeb266c2aee1cd8532f802e2c8cb",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/media/default.d2defeb2.png"
+  },
+  {
+    "revision": "e7c51d50b4f49144ac120faae7c4027c",
+    "url": "https://yonkoofthesea.github.io/csb-rzfpn/static/media/templateA.e7c51d50.png"
+  }
+]);
